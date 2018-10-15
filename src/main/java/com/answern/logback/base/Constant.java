07785e47d9f90f0bf8018fd20f15825e3fd06419
wemlive <674693934@qq.com>
2018-10-15 18:38:26 +0800

    注释掉不能提交的东西

--- src/main/java/com/answern/logback/base/Constant.java
+++ src/main/java/com/answern/logback/base/Constant.java
@@ -1,28 +1,28 @@
-package com.answern.logback.base;
-
-/**
- * 需求名称:
- * 类描述:[一句话描述该类的功能]<br/>
- *
- * @author [wem] <br/>
- * 创建时间:[2018/10/12 14:27]  <br/>
- * 版本:[v1.0]   <br/>
- */
-public class Constant {
-    /**
-     * 外网服务地址
-     */
-    public final static String QUEUE_ENDPOINT = "http://cmq-queue-bj.api.qcloud.com";
-
-    /**
-     * 用于标识 API 调用者身份
-     */
-    public final static String CMQ_SECRETID="AKIDbGrIbjMqpT5YIsSdnsmU0Kf8arwtqbP2";
-
-    /**
-     * 用于加密签名字符串和服务器端验证签名字符串的密钥
-     */
-    public final static String CMQ_SECRETKEY="qZL0f8QWZRDxZ2MYZP6g5QadVKWwSp14";
-    public final static String CMQ_QUEUENAME="loggerQueue";
-
-}
+//package com.answern.logback.base;
+//
+///**
+// * 需求名称:
+// * 类描述:[一句话描述该类的功能]<br/>
+// *
+// * @author [wem] <br/>
+// * 创建时间:[2018/10/12 14:27]  <br/>
+// * 版本:[v1.0]   <br/>
+// */
+//public class Constant {
+//    /**
+//     * 外网服务地址
+//     */
+//    public final static String QUEUE_ENDPOINT = "http://cmq-queue-bj.api.qcloud.com";
+//
+//    /**
+//     * 用于标识 API 调用者身份
+//     */
+//    public final static String CMQ_SECRETID="AKIDbGrIbjMqpT5YIsSdnsmU0Kf8arwtqbP2";
+//
+//    /**
+//     * 用于加密签名字符串和服务器端验证签名字符串的密钥
+//     */
+//    public final static String CMQ_SECRETKEY="qZL0f8QWZRDxZ2MYZP6g5QadVKWwSp14";
+//    public final static String CMQ_QUEUENAME="loggerQueue";
+//
+//}
